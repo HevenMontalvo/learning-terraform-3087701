e2342845d68acf876a328963036042e01910e8bc
HevenMontalvo <evn.montalvo@gmail.com>
2025-03-04 18:23:55 -0600

    fix

--- main.tf
+++ main.tf
@@ -36,31 +36,31 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
   from_port = 80
   to_port = 80
-  protoclo = "tcp"
+  protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group" "blog_https_in" {
+resource "aws_security_grou_rule" "blog_https_in" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
-  protoclo  = "tcp"
+  protocol  = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type = "egress"
   from_port = 0
   to_port = 0
-  protoclo = "-1"
+  protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
