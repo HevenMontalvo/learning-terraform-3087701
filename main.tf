ecb5961affcea4119fc4b2dafb14ce689e6dee32
HevenMontalvo <evn.montalvo@gmail.com>
2025-03-18 14:51:50 -0600

    module sg

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Data Sources
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -19,90 +18,75 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Módulo VPC
-module "blog_vpc" {
+module "module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
     Environment = "dev"
   }
-}
+} 
 
-# Recurso EC2
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
-# Módulo Security Group
+
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name    = "blog_new"
+  source = "terraform-aws-modules/security-group/aws"
 
-  vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  vpc_id      = data.aws_vpc.default.id
+  name        = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-# Módulo ALB
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
-  # Configuración del grupo de destino
-  target_groups = {
-    ex-instance = {
-      name_prefix = "blog-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-    }
-  }
-
-  # Configuración del listener
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-
-      default_action = {
-        type             = "forward"
-        target_group_key = "ex-instance"  # Referencia al grupo de destino
-      }
-    }
-  }
-
-  tags = {
-    Environment = "Development"
-    Project     = "Example"
-  }
+/*
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
-# Recurso Target Group Attachment
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = module.alb.target_groups["ex-instance"].arn
-  target_id        = aws_instance.blog.id
-  port             = 80
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
+
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+*/
